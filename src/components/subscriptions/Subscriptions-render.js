1224acf38244e470c6d4d14daae35b174b75e787
Mambans <persson96@outlook.com>
2019-08-09 21:37:32 +0200

    Moved videoImageUrls function to utilities file. Changed css classes to {styles...} (React class hash). Fixed .env file. Fixed requests, filtering and rendering of videos from multiple channels and cleaned up code/funcitons.

--- src/components/subscriptions/Subscriptions-render.js
+++ src/components/subscriptions/Subscriptions-render.js
@@ -1,111 +1,152 @@
 import React from "react";
-import Img from "react-image";
+
 import Moment from "react-moment";
 
-import "./Subscriptions.scss";
-import placeholderImg from "./../../assets/images/placeholder.png";
+import styles from "./Subscriptions.module.scss";
 import Utilities from "./../../utilities/utilities";
-import getSubscriptionVideos from "./Subscriptions";
-
-require("dotenv").config();
 
 const axios = require("axios");
 
 class Subscriptions extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { Videos: null };
+        this.state = { videos: null, loading: true, error: null };
         this.title = "Subscriptions";
     }
 
     async getSubscriptionVideos() {
-        console.log(1);
-        console.log(process.env.API_KEY);
+        const videos = [];
+        // eslint-disable-next-line
+        let channels = {
+            FastASMR: "UCHiof82PvgZrXFF-BRMvGDg",
+            ASMRGlow: "UCFmL725KKPx2URVPvH3Gp8w",
+            SiriusEyesASMR: "UCKzOQLe_60mhb0LPuKDSbCA",
+            LinusTechTips: "UCXuqSBlHAE6Xw-yeJA0Tunw",
+            GibiASMR: "UCE6acMV3m35znLcf0JGNn7Q",
+            // Techquickie: "UC0vBXGSyV14uvJ4hECDOl0Q",
+            // MovieClipsTrailers: "UCi8e0iOVk1fEOogdfu4YgfA",
+        };
+
+        let channelsArray = [
+            "UCHiof82PvgZrXFF-BRMvGDg",
+            "UCFmL725KKPx2URVPvH3Gp8w",
+            "UCKzOQLe_60mhb0LPuKDSbCA",
+            "UCXuqSBlHAE6Xw-yeJA0Tunw",
+            "UCE6acMV3m35znLcf0JGNn7Q",
+        ];
+
+        // try {
+        //     console.log("REQEUST SENT");
+
+        //     const response = await axios.get(
+        //         `https://www.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&channelId=${
+        //             channels.FastASMR
+        //         }&maxResults=10&key=${process.env.REACT_APP_API_KEY}`
+        //     );
+
+        //     console.log(response);
+        //     return response.data.items;
+        // } catch (error) {
+        //     console.error(error);
+        // }
 
         try {
-            console.log("REQEUST SENT");
+            await Promise.all(
+                channelsArray.map(async channel => {
+                    console.log("REQEUST SENT");
+                    console.log("channel: ", channel);
 
-            const response = await axios.get(
-                `https://www.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&channelId=UCHiof82PvgZrXFF-BRMvGDg&maxResults=10&key=${API_KEY}`
+                    const response = await axios.get(
+                        `https://www.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&channelId=${channel}&maxResults=5&key=${
+                            process.env.REACT_APP_API_KEY
+                        }`
+                    );
+
+                    let videosunfiltered = response.data.items.filter(video => {
+                        return video.snippet.type === "upload";
+                    });
+
+                    videosunfiltered.forEach(element => {
+                        videos.push(element);
+                    });
+                })
             );
-            console.log(response);
-            return response;
+
+            console.log("videos :", videos);
+            this.setState({
+                loading: false,
+                videos,
+            });
         } catch (error) {
             console.error(error);
+            this.setState({
+                loading: false,
+                error,
+            });
         }
-        console.log(2);
-    }
-
-    Subscriptions() {
-        this.componentDidMount();
-        this.render();
     }
 
     componentDidMount() {
-        getSubscriptionVideos();
-        this.prepareRendering();
-    }
-
-    truncate(input, max) {
-        if (input.length > max) return input.substring(0, max) + "..";
-        else return input;
-    }
-
-    async prepareRendering() {
-        let requestRes = await this.getSubscriptionVideos();
-
-        this.setState({
-            Videos: Object.values(requestRes.data.items).map(video => (
-                // console.log("sd", video.snippet.thumbnails.maxres.url),
-                <div className="video" key={video.contentDetails.upload.videoId}>
-                    <a
-                        className="video-img"
-                        href={
-                            `https://www.youtube.com/watch?v=` + video.contentDetails.upload.videoId
-                        }>
-                        <Img
-                            // src={[video.snippet.thumbnails.maxres.url, placeholderImg]}
-                            src={placeholderImg}
-                            alt="video-thumbnail"
-                            loader={
-                                <div className="loading-div">
-                                    <div className="lds-ripple">
-                                        <div />
-                                        <div />
-                                    </div>
-                                </div>
-                            }
-                        />
-                    </a>
-                    <h4 className="video-title">
-                        <a
-                            href={
-                                `https://www.youtube.com/watch?v=` +
-                                video.contentDetails.upload.videoId
-                            }>
-                            {Utilities.truncate(video.snippet.title, 50)}
-                        </a>
-                    </h4>
-                    <Moment className="video-date" fromNow>
-                        {video.snippet.publishedAt}
-                    </Moment>
-                    <p className="video-channel">
-                        <a href={`https://www.youtube.com/channel/` + video.snippet.channelId}>
-                            {video.snippet.channelTitle}
-                        </a>
-                    </p>
-                </div>
-            )),
-        });
+        this.getSubscriptionVideos();
     }
 
     render() {
-        return (
-            <>
-                <h2>{this.title}</h2>
-                <div className="video-container">{this.state.Videos}</div>
-            </>
-        );
+        const { error, loading, videos } = this.state;
+        if (error) {
+            return (
+                <div>
+                    <p>Error: {error.message}</p>
+                </div>
+            );
+        } else if (loading) {
+            return (
+                <div>
+                    <p>Loading..</p>
+                </div>
+            );
+        } else {
+            return (
+                <div className={styles.container}>
+                    {Object.values(videos).map(video => (
+                        // console.log("Thumnbails", video.snippet),
+                        <div className={styles.video} key={video.contentDetails.upload.videoId}>
+                            <a
+                                className={styles.img}
+                                href={
+                                    `https://www.youtube.com/watch?v=` +
+                                    video.contentDetails.upload.videoId
+                                }>
+                                <img
+                                    src={Utilities.videoImageUrls(video.snippet.thumbnails)}
+                                    // src={placeholderImg}
+                                    alt={styles.thumbnail}
+                                />
+                            </a>
+                            <h4 className={styles.title}>
+                                <a
+                                    href={
+                                        `https://www.youtube.com/watch?v=` +
+                                        video.contentDetails.upload.videoId
+                                    }>
+                                    {Utilities.truncate(video.snippet.title, 50)}
+                                </a>
+                            </h4>
+                            <Moment className={styles.date} fromNow>
+                                {video.snippet.publishedAt}
+                            </Moment>
+                            <p className={styles.channel}>
+                                <a
+                                    href={
+                                        `https://www.youtube.com/channel/` + video.snippet.channelId
+                                    }>
+                                    {video.snippet.channelTitle}
+                                </a>
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            );
+        }
     }
 }
 
--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,7 +1,23 @@
+import placeholderImg from "./../assets/images/placeholder.png";
+
 const Utilities = {
     truncate: function(input, max) {
         if (input.length > max) return input.substring(0, max) + "..";
         else return input;
+    },
+
+    videoImageUrls(urls) {
+        if (urls.maxres) {
+            return urls.maxres.url;
+        } else if (urls.standard) {
+            return urls.standard.url;
+        } else if (urls.high) {
+            return urls.high.url;
+        } else if (urls.medium) {
+            return urls.medium.url;
+        } else {
+            return placeholderImg;
+        }
     },
 };
 
