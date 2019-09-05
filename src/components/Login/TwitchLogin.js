adca7874314e22553c682cf8378f13ad61520f0d
Mambans <persson96@outlook.com>
2019-09-05 17:50:31 +0200

    Cleanup console.logs and comments.

--- src/components/Login/TwitchLogin.js
+++ src/components/Login/TwitchLogin.js
@@ -8,52 +8,48 @@ import axios from "axios";
 import { AuthContext } from "components/Login/AuthContextProvider";
 
 class Auth extends React.Component {
-    constructor(props) {
-        super(props);
-        this.title = "Login";
-    }
+  constructor(props) {
+    super(props);
+    this.title = "Login";
+  }
 
-    async getAccessToken() {
-        const { setLoggedIn } = this.context;
-        console.log("getAccessToken");
-
-        const url = new URL(window.location.href);
-        console.log("TCL: Auth -> getAccessToken -> url", url);
-        const authCode = url.searchParams.get("code");
-        console.log("TCL: Auth -> getAccessToken -> authCode", authCode);
-        const response = await axios.post(`https://id.twitch.tv/oauth2/token
+  async getAccessToken() {
+    const { setLoggedIn } = this.context;
+    const url = new URL(window.location.href);
+    const authCode = url.searchParams.get("code");
+    const response = await axios.post(`https://id.twitch.tv/oauth2/token
 ?client_id=${process.env.REACT_APP_TWITCH_CLIENT_ID}
 &client_secret=${process.env.REACT_APP_TWITCH_SECRET}
 &code=${authCode}
 &grant_type=authorization_code
 &redirect_uri=http://localhost:3000/twitch/auth`);
 
-        localStorage.setItem("access_token", response.data.access_token);
+    localStorage.setItem("access_token", response.data.access_token);
 
-        setLoggedIn(true);
+    setLoggedIn(true);
+  }
+
+  componentDidMount() {
+    this.getAccessToken();
+  }
+
+  render() {
+    const { loggedIn } = this.context;
+    if (loggedIn) {
+      return (
+        <Alert variant="success" style={Utilities.alertWarning}>
+          <Alert.Heading>Successfully logged in</Alert.Heading>
+          <hr />
+        </Alert>
+      );
     }
 
-    componentDidMount() {
-        this.getAccessToken();
-    }
-
-    render() {
-        const { loggedIn } = this.context;
-        if (loggedIn) {
-            return (
-                <Alert variant="success" style={Utilities.alertWarning}>
-                    <Alert.Heading>Successfully logged in</Alert.Heading>
-                    <hr />
-                </Alert>
-            );
-        }
-
-        return (
-            <Spinner animation="border" role="status" style={Utilities.loadingSpinner}>
-                <span className="sr-only">Loading...</span>
-            </Spinner>
-        );
-    }
+    return (
+      <Spinner animation="border" role="status" style={Utilities.loadingSpinner}>
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
+  }
 }
 
 Auth.contextType = AuthContext;
--- src/components/Login/YoutubeLogin.js
+++ src/components/Login/YoutubeLogin.js
@@ -8,69 +8,57 @@ import Utilities from "utilities/utilities";
 import { AuthContext } from "components/Login/AuthContextProvider";
 
 class youtubeAuth extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { loggedIn: false };
-        this.title = "Login";
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false };
+    this.title = "Login";
+  }
+
+  async access() {
+    this.setState({
+      loggedIn: true,
+    });
+  }
+
+  async getAccessToken() {
+    const url = new URL(window.location.href).hash;
+
+    const authCode = url
+      .split("#")[1]
+      .split("&")[0]
+      .slice(13);
+
+    localStorage.setItem("Youtube-access_token", authCode);
+
+    this.setState({
+      loggedIn: true,
+    });
+  }
+
+  componentDidMount() {
+    this.getAccessToken();
+  }
+
+  render() {
+    const { loggedIn } = this.state;
+    if (loggedIn) {
+      return (
+        <>
+          <Alert variant="success" style={Utilities.alertWarning}>
+            <Alert.Heading>Successfully logged in</Alert.Heading>
+            <hr />
+          </Alert>
+        </>
+      );
     }
-
-    async access() {
-        this.setState({
-            loggedIn: true,
-        });
-    }
-
-    async getAccessToken() {
-        // const { setLoggedIn } = this.context;
-        console.log("getAccessToken");
-
-        const url = new URL(window.location.href).hash;
-        console.log("url: ", url);
-
-        // const authCode = url.searchParams.get("#access_token");
-        const authCode = url
-            .split("#")[1]
-            .split("&")[0]
-            .slice(13);
-
-        // const asd = authCode.slice(13);
-        // console.log("TCL: youtubeAuth -> getAccessToken -> asd", asd);
-
-        console.log("authCode: ", authCode);
-
-        localStorage.setItem("Youtube-access_token", authCode);
-
-        this.setState({
-            loggedIn: true,
-        });
-    }
-
-    componentDidMount() {
-        this.getAccessToken();
-    }
-
-    render() {
-        const { loggedIn } = this.state;
-        if (loggedIn) {
-            return (
-                <>
-                    <Alert variant="success" style={Utilities.alertWarning}>
-                        <Alert.Heading>Successfully logged in</Alert.Heading>
-                        <hr />
-                    </Alert>
-                </>
-            );
-        }
-        return (
-            <>
-                <Spinner animation="border" role="status" style={Utilities.loadingSpinner}>
-                    <span className="sr-only">Loading...</span>
-                </Spinner>
-            </>
-        );
-    }
+    return (
+      <>
+        <Spinner animation="border" role="status" style={Utilities.loadingSpinner}>
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      </>
+    );
+  }
 }
-
-// Auth.contextType = AuthContext;
 
 export default youtubeAuth;
