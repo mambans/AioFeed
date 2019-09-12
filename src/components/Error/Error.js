7b2db2d73e75a96038ebb8336eff5e23e4515762
Mambans <persson96@outlook.com>
2019-09-12 23:16:19 +0200

    Refactored Error component to react hooks.

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,52 +1,85 @@
 import Alert from "react-bootstrap/Alert";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Utilities from "./../../utilities/Utilities";
 
-class ErrorHandeling extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasError: false,
-      error: this.props.data,
-      info: null,
-      errorTitle: "Oh-oh! Something bad happened.",
-    };
-  }
+function ErrorHandeling(data) {
+  const [errorMessage, setErrorMessage] = useState(data.data.message);
+  const [errorTitle, setErrorTitle] = useState();
 
-  ErrorHandelingText() {
-    let error;
+  useEffect(() => {
+    async function handleErrorContent() {
+      let error;
 
-    switch (this.props.data.message) {
-      case "Network Error":
-        error = this.props.data.message + " -Database server may be offline.";
-        break;
-      default:
-        error = this.props.data.message;
+      switch (data.data.message) {
+        case "Network Error":
+          error = data.data.message + " -Database server may be offline.";
+          break;
+        default:
+          error = data.data.message;
+          break;
+      }
+
+      setErrorMessage(error);
+
+      setErrorTitle(data.data.title ? data.data.title : "Oh-oh! Something bad happened.");
     }
+    handleErrorContent();
+  }, [data.data.message, data.data.title, errorMessage]);
 
-    this.setState({
-      error,
-      errorTitle: this.props.data.title ? this.props.data.title : "Oh-oh! Something bad happened.",
-    });
-  }
-
-  componentDidMount() {
-    console.log("fgh: ", this.props.data);
-
-    this.ErrorHandelingText();
-  }
-
-  render() {
-    const { error, errorTitle } = this.state;
-    return (
-      <Alert variant="warning" style={Utilities.alertWarning}>
-        <Alert.Heading>{errorTitle}</Alert.Heading>
-        <hr />
-        {error.toString()}
-      </Alert>
-    );
-  }
+  return (
+    <Alert variant="warning" style={Utilities.alertWarning}>
+      <Alert.Heading>{errorTitle}</Alert.Heading>
+      <hr />
+      {errorMessage.toString()}
+    </Alert>
+  );
 }
+
+// class ErrorHandeling extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       hasError: false,
+//       error: this.props.data,
+//       info: null,
+//       errorTitle: "Oh-oh! Something bad happened.",
+//     };
+//   }
+
+//   ErrorHandelingText() {
+//     let error;
+
+//     switch (this.props.data.message) {
+//       case "Network Error":
+//         error = this.props.data.message + " -Database server may be offline.";
+//         break;
+//       default:
+//         error = this.props.data.message;
+//     }
+
+//     this.setState({
+//       error,
+//       errorTitle: this.props.data.title ? this.props.data.title : "Oh-oh! Something bad happened.",
+//     });
+//   }
+
+//   componentDidMount() {
+//     console.log("fgh: ", this.props.data);
+
+//     this.ErrorHandelingText();
+//   }
+
+//   render() {
+//     const { error, errorTitle } = this.state;
+//     return (
+//       <Alert variant="warning" style={Utilities.alertWarning}>
+//         <Alert.Heading>{errorTitle}</Alert.Heading>
+//         <hr />
+//         {error.toString()}
+//       </Alert>
+//     );
+//   }
+// }
 
 export default ErrorHandeling;
