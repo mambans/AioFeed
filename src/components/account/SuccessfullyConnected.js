8fbc50c4413d2bf9e0e1ba5e4b35461bcc293a5a
Mambans <persson96@outlook.com>
2019-09-28 18:39:28 +0200

    Added close button, and auto close after 3s on SuccessfullyConnected alerts.

--- src/components/account/SuccessfullyConnected.js
+++ src/components/account/SuccessfullyConnected.js
@@ -1,35 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Alert } from "react-bootstrap";
+import { Animated } from "react-animated-css";
 
 import Utilities from "utilities/Utilities";
 
 function SuccessfullyConnected() {
+  const [show, setShow] = useState(true);
   const url = new URL(window.location.href);
 
-  if (url.searchParams.get("TwitchloggedIn") && Utilities.getCookie("Twitch-access_token")) {
-    return (
-      <>
-        <Alert variant='success' style={Utilities.alertWarning}>
-          <Alert.Heading>Successfully connected with Twitch!</Alert.Heading>
-          <hr />
-        </Alert>
-      </>
-    );
-  } else if (
-    url.searchParams.get("YoutubeloggedIn") &&
-    Utilities.getCookie("Youtube-access_token")
-  ) {
-    return (
-      <>
-        <Alert variant='success' style={Utilities.alertWarning}>
-          <Alert.Heading>Successfully connected with Youtube!</Alert.Heading>
-          <hr />
-        </Alert>
-      </>
-    );
-  } else {
-    return null;
+  useEffect(() => {
+    setTimeout(function() {
+      setShow(false);
+    }, 3000);
+  }, []);
+
+  if (show) {
+    if (url.searchParams.get("TwitchConnected") && Utilities.getCookie("Twitch-access_token")) {
+      return (
+        <Animated
+          animationIn='fadeIn'
+          animationOut='fadeOut'
+          animationOutDelay={2000}
+          animationOutDuration={1000}
+          isVisible={false}>
+          <Alert
+            variant='success'
+            style={Utilities.alertWarning}
+            onClose={() => setShow(false)}
+            dismissible>
+            <Alert.Heading>Successfully connected with Twitch!</Alert.Heading>
+            <hr />
+          </Alert>
+        </Animated>
+      );
+    } else if (
+      url.searchParams.get("YoutubeConnected") &&
+      Utilities.getCookie("Youtube-access_token")
+    ) {
+      return (
+        <Animated
+          animationIn='fadeIn'
+          animationOut='fadeOut'
+          animationOutDelay={2000}
+          animationOutDuration={1000}
+          isVisible={false}>
+          <Alert
+            variant='success'
+            style={Utilities.alertWarning}
+            onClose={() => setShow(false)}
+            dismissible>
+            <Alert.Heading>Successfully connected with Youtube!</Alert.Heading>
+            <hr />
+          </Alert>
+        </Animated>
+      );
+    } else {
+      return "";
+    }
   }
+  return "";
 }
 
 export default SuccessfullyConnected;
