b62dbd9f1ac8a0bba3479598a793d40c8b89c32d
Mambans <persson96@outlook.com>
2019-10-24 22:50:04 +0200

    Added icons to the footer links.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,6 +2,13 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 
+import Icon from "react-icons-kit";
+import { github } from "react-icons-kit/icomoon/github";
+import { home } from "react-icons-kit/icomoon/home";
+import { feed } from "react-icons-kit/icomoon/feed";
+import { ic_account_circle } from "react-icons-kit/md/ic_account_circle";
+import { email } from "react-icons-kit/entypo/email";
+
 import style from "./Footer.module.scss";
 
 const Footer = () => {
@@ -11,22 +18,29 @@ const Footer = () => {
         <ul>
           <li>
             <Nav.Link as={NavLink} to='/'>
+              <Icon icon={home} size={20} style={{ paddingRight: "0.75rem" }}></Icon>
               Home
             </Nav.Link>
           </li>
           <li>
+            <Nav.Link as={NavLink} to='/feed'>
+              <Icon icon={feed} size={20} style={{ paddingRight: "0.75rem" }}></Icon>
+              Feed
+            </Nav.Link>
+          </li>
+          <li>
             <Nav.Link as={NavLink} to='/account'>
+              <Icon icon={ic_account_circle} size={20} style={{ paddingRight: "0.75rem" }}></Icon>
               Account
             </Nav.Link>
           </li>
           <li>
             <Nav.Link as={NavLink} to='/account/create'>
+              <Icon
+                icon={ic_account_circle}
+                size={20}
+                style={{ paddingRight: "0.75rem", color: "grey" }}></Icon>
               Create account
-            </Nav.Link>
-          </li>
-          <li>
-            <Nav.Link as={NavLink} to='/account/login'>
-              Login
             </Nav.Link>
           </li>
         </ul>
@@ -36,10 +50,10 @@ const Footer = () => {
         <p>Test text</p>
         <p className={style.centerBottomText}>
           <Nav.Link as={NavLink} to='/legality#Conditions'>
-            -Conditions of Use
+              Conditions of Use
           </Nav.Link>
           <Nav.Link as={NavLink} to='/legality#Privacy'>
-            -Privacy Notice
+            Privacy Notice
           </Nav.Link>
           © 2020 Skåne Sweden, Robin Persson. No rights reserved.
         </p>
@@ -47,7 +61,11 @@ const Footer = () => {
       <div>
         <ul>
           <li>
-            <a href='https://github.com/mambans/Notifies'>Github-Notifies</a>
+            <a href='https://github.com/mambans/Notifies'>
+              {" "}
+              <Icon icon={github} size={20} style={{ paddingRight: "0.75rem" }}></Icon>
+              Github-Notifies
+            </a>
           </li>
           <li>
             <button
@@ -55,6 +73,7 @@ const Footer = () => {
               onClick={() => {
                 window.open("mailto:perssons1996@gmail.com?subject=subject&body=body");
               }}>
+              <Icon icon={email} size={20} style={{ paddingRight: "0.75rem" }}></Icon>
               Email
             </button>
           </li>
