25924092d2f1ca58b8e8086f01cfe9d94c4e9a29
mambans <perssons1996@gmail.com>
2019-12-07 15:38:11 +0100

    Fixed "clear (x)" button not visible when 0 notifications.

--- frontend/src/components/notifications/RenderNotifications.js
+++ frontend/src/components/notifications/RenderNotifications.js
@@ -12,48 +12,48 @@ export default () => {
       {props => {
         return (
           <div>
-            {props.notifications ? (
-              <ul>
-                <li
-                  onClick={() => {
-                    props.clearNotifications();
-                  }}
-                  style={{
-                    cursor: "pointer",
-                    display: "flex",
-                    justifyContent: "center",
-                    fontWeight: "bold",
-                  }}>
-                  Clear all ({props.notifications.length})
-                </li>
-                {props.notifications.map(item => {
-                  return (
-                    <Notification key={item.key}>
-                      <a
-                        className='profileImg'
-                        href={"https://www.twitch.tv/" + item.user_name.toLowerCase()}
-                        alt=''>
-                        <img src={item.profile_img_url} alt=''></img>
-                      </a>
-                      <a
-                        href={"https://www.twitch.tv/" + item.user_name.toLowerCase()}
-                        alt=''
-                        className='name'>
-                        <b>{item.user_name}</b> went {item.status}
-                      </a>
-                      <a
-                        href={"https://www.twitch.tv/" + item.user_name.toLowerCase() + "/videos"}
-                        className='title'>
-                        {Utilities.truncate(item.title, 50)}
-                      </a>
-                      <Moment fromNow className='date'>
-                        {item.date}
-                      </Moment>
-                    </Notification>
-                  );
-                })}
-              </ul>
-            ) : null}
+            <ul>
+              <li
+                onClick={() => {
+                  props.clearNotifications();
+                }}
+                style={{
+                  cursor: "pointer",
+                  display: "flex",
+                  justifyContent: "center",
+                  fontWeight: "bold",
+                }}>
+                Clear all ({props.notifications ? props.notifications.length : 0})
+              </li>
+              {props.notifications
+                ? props.notifications.map(item => {
+                    return (
+                      <Notification key={item.key}>
+                        <a
+                          className='profileImg'
+                          href={"https://www.twitch.tv/" + item.user_name.toLowerCase()}
+                          alt=''>
+                          <img src={item.profile_img_url} alt=''></img>
+                        </a>
+                        <a
+                          href={"https://www.twitch.tv/" + item.user_name.toLowerCase()}
+                          alt=''
+                          className='name'>
+                          <b>{item.user_name}</b> went {item.status}
+                        </a>
+                        <a
+                          href={"https://www.twitch.tv/" + item.user_name.toLowerCase() + "/videos"}
+                          className='title'>
+                          {Utilities.truncate(item.title, 50)}
+                        </a>
+                        <Moment fromNow className='date'>
+                          {item.date}
+                        </Moment>
+                      </Notification>
+                    );
+                  })
+                : null}
+            </ul>
           </div>
         );
       }}
