5e84203dd16d17be58b6542c7ac558d5e62ad28f
mambans <perssons1996@gmail.com>
2020-03-15 02:54:09 +0100

    Moved fetchUptime() axios request config to a axiosConfig obj.

--- frontend/src/components/twitch/player/fetchUptime.js
+++ frontend/src/components/twitch/player/fetchUptime.js
@@ -10,39 +10,31 @@ import Util from "./../../../util/Util";
  * @async
  */
 export default async (twitchPlayer, setUptime, uptimeTimer) => {
-  await axios
-    .get(`https://api.twitch.tv/helix/streams`, {
-      params: {
-        user_id: twitchPlayer.current.getChannelId(),
-        first: 1,
-      },
-      headers: {
-        Authorization: `Bearer ${Util.getCookie("Twitch-access_token")}`,
-        "Client-ID": process.env.REACT_APP_TWITCH_CLIENT_ID,
-      },
-    })
+  const axiosConfig = {
+    method: "get",
+    url: `https://api.twitch.tv/helix/streams`,
+    params: {
+      user_id: twitchPlayer.current.getChannelId(),
+      first: 1,
+    },
+    headers: {
+      Authorization: `Bearer ${Util.getCookie("Twitch-access_token")}`,
+      "Client-ID": process.env.REACT_APP_TWITCH_CLIENT_ID,
+    },
+  };
+
+  await axios(axiosConfig)
     .then(res => {
       if (res.data.data[0] && res.data.data[0].started_at) {
         setUptime(res.data.data[0].started_at);
       } else {
         uptimeTimer.current = setInterval(async () => {
-          await axios
-            .get(`https://api.twitch.tv/helix/streams`, {
-              params: {
-                user_id: twitchPlayer.current.getChannelId(),
-                first: 1,
-              },
-              headers: {
-                Authorization: `Bearer ${Util.getCookie("Twitch-access_token")}`,
-                "Client-ID": process.env.REACT_APP_TWITCH_CLIENT_ID,
-              },
-            })
-            .then(res => {
-              if (res.data.data[0] && res.data.data[0].started_at) {
-                setUptime(res.data.data[0].started_at);
-                clearInterval(uptimeTimer.current);
-              }
-            });
+          await axios(axiosConfig).then(res => {
+            if (res.data.data[0] && res.data.data[0].started_at) {
+              setUptime(res.data.data[0].started_at);
+              clearInterval(uptimeTimer.current);
+            }
+          });
         }, 1000 * 30);
       }
     })
