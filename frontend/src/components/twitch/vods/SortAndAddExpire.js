1bd20940b5f1aed0cb56eb1fb57da54630ab75fc
mambans <perssons1996@gmail.com>
2020-03-27 18:07:41 +0100

    Small renaming if variables.

--- frontend/src/components/twitch/vods/SortAndAddExpire.js
+++ frontend/src/components/twitch/vods/SortAndAddExpire.js
@@ -4,17 +4,20 @@ export default (followedStreamVods, vodExpire, oldLoaded, oldExpire) => {
   const liveVods = followedStreamVods.filter(vod => {
     return vod.thumbnail_url === "";
   });
+
   const completedVods = followedStreamVods.filter(vod => {
     return vod.thumbnail_url !== "";
   });
-  const asd1 = sortBy(
+  const sortedLiveVods = sortBy(
     liveVods,
     stream => new Date().getTime() - new Date(stream.created_at).getTime()
   );
-  const asd2 = reverse(sortBy(completedVods, stream => new Date(stream.endDate).getTime()));
+  const sortedCompletedVods = reverse(
+    sortBy(completedVods, stream => new Date(stream.endDate).getTime())
+  );
 
   const Vods = {
-    data: asd1.concat(asd2),
+    data: sortedLiveVods.concat(sortedCompletedVods),
     expire: oldExpire || new Date().setHours(new Date().getHours() + vodExpire),
     loaded: oldLoaded || new Date(),
   };
