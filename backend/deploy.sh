9d3d53f62d2f0c01cdc08dc3b5c6102e746a4b4a
mambans <perssons1996@gmail.com>
2020-04-22 03:53:57 +0200

    Added start time, end time  and cancel on error  for backend deploy script.

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,2 +1,6 @@
+echo "Started: `date`"
+set -e
 aws cloudformation package --template-file cloudformation.yaml --s3-bucket aiofeed-backend --output-template-file cloudformation-packaged.yaml
 aws cloudformation deploy --template-file cloudformation-packaged.yaml --stack-name aiofeed-backend --capabilities CAPABILITY_IAM
+
+echo "Done: `date`"
