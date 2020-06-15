bf9566401636a42b29c6c4a20275d29ef95db6e3
mambans <perssons1996@gmail.com>
2020-06-15 04:28:30 +0200

    For validateToken() added checking if an access_token exists else return false.

--- frontend/src/components/twitch/validateToken.js
+++ frontend/src/components/twitch/validateToken.js
@@ -3,38 +3,41 @@ import { getCookie, AddCookie } from "../../util/Utils";
 import reauthenticate from "./reauthenticate";
 
 export default async (forceRun = false) => {
-  if (
-    forceRun ||
-    !getCookie("Twitch_token_validated") ||
-    getCookie("Twitch_token_validated") < Date.now()
-  ) {
-    const expireDate = new Date(Date.now() + 5000);
-    AddCookie("Twitch_token_validated", expireDate.getTime(), expireDate);
-    return await axios
-      .get("https://id.twitch.tv/oauth2/validate", {
-        headers: {
-          Authorization: `OAuth ${getCookie("Twitch-access_token")}`,
-        },
-      })
-      .then((res) => {
-        const expireDate = new Date(Date.now() + 60000);
-        AddCookie("Twitch_token_validated", expireDate.getTime(), expireDate);
-        if (
-          res.data.client_id === process.env.REACT_APP_TWITCH_CLIENT_ID &&
-          res.data.login === getCookie("Twitch-username").toLocaleLowerCase()
-        ) {
-          return res.data;
-        }
-        console.warn("Token details DID NOT match.");
-        return reauthenticate();
-      })
-      .catch((error) => {
-        // console.error("error", error.response.statusText);
-        // console.error("error status", error.response.status);
-        console.log("Invalid token");
-        return reauthenticate();
-      });
-  } else {
+  if (getCookie("Twitch-access_token")) {
+    if (
+      forceRun ||
+      !getCookie("Twitch_token_validated") ||
+      getCookie("Twitch_token_validated") < Date.now()
+    ) {
+      const expireDate = new Date(Date.now() + 5000);
+      AddCookie("Twitch_token_validated", expireDate.getTime(), expireDate);
+      return await axios
+        .get("https://id.twitch.tv/oauth2/validate", {
+          headers: {
+            Authorization: `OAuth ${getCookie("Twitch-access_token")}`,
+          },
+        })
+        .then((res) => {
+          const expireDate = new Date(Date.now() + 60000);
+          AddCookie("Twitch_token_validated", expireDate.getTime(), expireDate);
+          if (
+            res.data.client_id === process.env.REACT_APP_TWITCH_CLIENT_ID &&
+            res.data.login === getCookie("Twitch-username").toLocaleLowerCase()
+          ) {
+            return res.data;
+          }
+          console.warn("Token details DID NOT match.");
+          return reauthenticate();
+        })
+        .catch((error) => {
+          // console.error("error", error.response.statusText);
+          // console.error("error status", error.response.status);
+          console.log("Invalid token");
+          return reauthenticate();
+        });
+    }
+
     return true;
   }
+  return false;
 };
--- frontend/src/components/youtube/validateToken.js
+++ frontend/src/components/youtube/validateToken.js
@@ -6,23 +6,25 @@ export default async ({ authKey }) => {
   const expireDate = new Date(Date.now() + 20000);
   const access_token = getCookie("Youtube-access_token");
 
-  if (!getCookie("Youtube_token_validated")) {
-    await axios
-      .post(`https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${access_token}`)
-      .then((res) => {
-        console.log("Valid YouTube Access_token");
-        AddCookie("Youtube_token_validated", true, expireDate);
-        return res.data;
-      })
-      .catch((error) => {
-        console.log("Invalid YouTube Access_token");
-        return autoReauthenticate({ authKey }).then((res) => {
-          console.log("New YouTube Access_token fetched");
+  if (!access_token) {
+    if (!getCookie("Youtube_token_validated")) {
+      await axios
+        .post(`https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${access_token}`)
+        .then((res) => {
+          console.log("Valid YouTube Access_token");
           AddCookie("Youtube_token_validated", true, expireDate);
-          return res;
+          return res.data;
+        })
+        .catch((error) => {
+          console.log("Invalid YouTube Access_token");
+          return autoReauthenticate({ authKey }).then((res) => {
+            console.log("New YouTube Access_token fetched");
+            AddCookie("Youtube_token_validated", true, expireDate);
+            return res;
+          });
         });
-      });
-  } else {
+    }
     return true;
   }
+  return false;
 };
