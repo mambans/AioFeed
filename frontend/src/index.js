9ba03ee6d2bf687b3896ae774425004df63cc166
mambans <perssons1996@gmail.com>
2020-06-16 17:06:37 +0200

    Removed importing of regular Bootstrap from index.html as a <link> and changed to import it in index.js as a npm dependency.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import "./transitions.scss";
-import "./components/themes/Themes.scss";
-import App from "./components/app/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
+import './transitions.scss';
+import './components/themes/Themes.scss';
+import App from './components/app/App';
 
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
