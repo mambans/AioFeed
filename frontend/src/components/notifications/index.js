c892b153f881020c25f4f9ff29d87807b4403262
mambans <perssons1996@gmail.com>
2020-06-29 23:17:17 +0200

    Changed unseen notifications count to show "99+" when above 100. Added some ES2020 features.

--- frontend/src/components/notifications/index.js
+++ frontend/src/components/notifications/index.js
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom";
-import { MdNotifications } from "react-icons/md";
-import { MdNotificationsNone } from "react-icons/md";
-import Modal from "react-bootstrap/Modal";
-import React, { useState, useContext } from "react";
+import { Link } from 'react-router-dom';
+import { MdNotifications } from 'react-icons/md';
+import { MdNotificationsNone } from 'react-icons/md';
+import Modal from 'react-bootstrap/Modal';
+import React, { useState, useContext } from 'react';
 
-import { ButtonList } from "./../sharedStyledComponents";
-import { Notification, Date, NotificationListContainer } from "./styledComponent";
-import { truncate } from "../../util/Utils";
-import { UnseenNotifcationCount } from "./../notifications/styledComponent";
-import NavigationContext from "./../navigation/NavigationContext";
-import NotificationsContext from "./../notifications/NotificationsContext";
-import styles from "./Notifications.module.scss";
+import { ButtonList } from './../sharedStyledComponents';
+import { Notification, Date, NotificationListContainer } from './styledComponent';
+import { truncate } from '../../util/Utils';
+import { UnseenNotifcationCount } from './../notifications/styledComponent';
+import NavigationContext from './../navigation/NavigationContext';
+import NotificationsContext from './../notifications/NotificationsContext';
+import styles from './Notifications.module.scss';
 
 export default () => {
   const [show, setShow] = useState(false);
@@ -35,34 +35,35 @@ export default () => {
       <ButtonList
         onClick={handleShow}
         style={{
-          border: "none",
-          background: "none",
-          boxShadow: "none",
-          padding: "0",
+          border: 'none',
+          background: 'none',
+          boxShadow: 'none',
+          padding: '0',
         }}
-        title='Notifications'>
-        {unseenNotifications &&
-        Array.isArray(unseenNotifications) &&
-        unseenNotifications.length > 0 ? (
+        title='Notifications'
+      >
+        {unseenNotifications?.length > 0 ? (
           <>
             <MdNotifications
-              size={shrinkNavbar === "true" ? 35 : 40}
+              size={shrinkNavbar === 'true' ? 35 : 40}
               style={{
-                color: "var(--newHighlightColor)",
-                alignItems: "center",
-                display: "flex",
-                transition: "ease-in-out 1s",
+                color: 'var(--newHighlightColor)',
+                alignItems: 'center',
+                display: 'flex',
+                transition: 'ease-in-out 1s',
               }}
             />
-            <UnseenNotifcationCount>{unseenNotifications.length}</UnseenNotifcationCount>
+            <UnseenNotifcationCount>
+              {unseenNotifications?.length > 100 ? '99+' : unseenNotifications?.length}
+            </UnseenNotifcationCount>
           </>
         ) : (
           <MdNotificationsNone
-            size={shrinkNavbar === "true" ? 35 : 40}
+            size={shrinkNavbar === 'true' ? 35 : 40}
             style={{
-              alignItems: "center",
-              display: "flex",
-              ransition: "ease-in-out 1s",
+              alignItems: 'center',
+              display: 'flex',
+              ransition: 'ease-in-out 1s',
             }}
           />
         )}
@@ -73,45 +74,43 @@ export default () => {
         onHide={handleClose}
         dialogClassName={styles.modal}
         backdropClassName={styles.modalBackdrop}
-        style={{ minWidth: "unset", maxWidth: "unset" }}>
+        style={{ minWidth: 'unset', maxWidth: 'unset' }}
+      >
         <NotificationListContainer>
           <li
             id='clear'
             onClick={() => {
               clearNotifications();
-            }}>
-            Clear all ({notifications ? notifications.length : 0})
+            }}
+          >
+            Clear all ({notifications?.length || 0})
           </li>
-          {notifications &&
-            notifications.map((item) => {
-              return (
-                <Notification key={item.key} status={item.notiStatus}>
-                  <Link
-                    to={`/${item.user_name.toLowerCase()}/channel`}
-                    className='profileImg'
-                    alt=''>
-                    <img src={item.profile_img_url} alt=''></img>
+          {notifications?.map((item) => {
+            return (
+              <Notification key={item.key} status={item.notiStatus}>
+                <Link to={`/${item.user_name.toLowerCase()}/channel`} className='profileImg' alt=''>
+                  <img src={item.profile_img_url} alt=''></img>
+                </Link>
+                <div className='textContainer'>
+                  <Link to={`/${item.user_name.toLowerCase()}/channel`} className='name'>
+                    <b>{item.user_name}</b> {item.notiStatus}
                   </Link>
-                  <div className='textContainer'>
-                    <Link to={`/${item.user_name.toLowerCase()}/channel`} className='name'>
-                      <b>{item.user_name}</b> {item.notiStatus}
-                    </Link>
-                    <Link to={`/${item.user_name.toLowerCase()}/channel`} className='title'>
-                      {item.notiStatus.includes("updated") && item.text
-                        ? item.text.split("\n").map((line) => {
-                            return (
-                              <p className='UpdateText' key={line}>
-                                {line}
-                              </p>
-                            );
-                          })
-                        : truncate(item.title, 30)}
-                    </Link>
-                    <Date date={item.date} status={item.notiStatus} />
-                  </div>
-                </Notification>
-              );
-            })}
+                  <Link to={`/${item.user_name.toLowerCase()}/channel`} className='title'>
+                    {(item.notiStatus.includes('updated') &&
+                      item?.text?.split('\n').map((line) => {
+                        return (
+                          <p className='UpdateText' key={line}>
+                            {line}
+                          </p>
+                        );
+                      })) ||
+                      truncate(item.title, 30)}
+                  </Link>
+                  <Date date={item.date} status={item.notiStatus} />
+                </div>
+              </Notification>
+            );
+          })}
         </NotificationListContainer>
       </Modal>
     </>
