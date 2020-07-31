7d8b09d80882c143c6aaaa5a0aec258c1ef84532
mambans <perssons1996@gmail.com>
2020-08-01 00:34:12 +0200

    Fixed <LoadingList> to not re-render (render twice) during loading.

--- frontend/src/components/twitch/categoryTopStreams/LoadingList.js
+++ frontend/src/components/twitch/categoryTopStreams/LoadingList.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { StyledLoadingListElement } from './styledComponents';
 
 export default ({ amount, style = {} }) => {
-  const array = Array.apply(null, Array(amount)).map(function (x, i) {
-    return i;
-  });
+  const array = useRef(
+    Array.apply(null, Array(amount)).map(function (x, i) {
+      return { id: i, width: `${Math.floor(Math.random() * 80)}%` };
+    })
+  );
 
-  return array.map((item, index) => {
+  return array.current.map((item, index) => {
     return (
       <StyledLoadingListElement key={index} style={{ ...style }}>
-        <div style={{ width: `${Math.floor(Math.random() * 80)}%` }}></div>
+        <div style={{ width: item.width }}></div>
       </StyledLoadingListElement>
     );
   });
