6a9dfe2d7afcebd06acd3ba888942451a5b07bc7
mambans <perssons1996@gmail.com>
2020-08-01 00:17:03 +0200

    Cleaned up code.

--- frontend/src/components/twitch/live/CountdownCircleTimer.js
+++ frontend/src/components/twitch/live/CountdownCircleTimer.js
@@ -1,28 +1,8 @@
-import { StyledCountdownCircle } from "./../StyledComponents";
-import React, { useState, useEffect } from "react";
+import { StyledCountdownCircle } from './../StyledComponents';
+import React, { useState, useEffect } from 'react';
 
 export default ({ startDuration }) => {
   const [countdown, setCountdown] = useState(25);
-  // const [delay, setDelay] = useState(0);
-
-  // useEffect(() => {
-  //   console.log("-11");
-  //   const windowFocusHandler = () => {
-  //     console.log("-22");
-  //     console.log("windowFocusHandler -> -25 + countdown", -25 + countdown);
-  //     if (true) {
-  //       console.log(true);
-  //       // setDelay(-25 + countdown);
-  //       setDelay(countdown);
-  //     }
-  //   };
-
-  //   window.addEventListener("focus", windowFocusHandler);
-
-  //   return () => {
-  //     window.removeEventListener("focus", windowFocusHandler);
-  //   };
-  // }, [countdown]);
 
   useEffect(() => {
     setCountdown(startDuration);
@@ -39,11 +19,7 @@ export default ({ startDuration }) => {
   }, [startDuration]);
 
   return (
-    <StyledCountdownCircle
-    // countdown={countdown}
-    // delay={delay}
-    // startDuration={startDuration}
-    >
+    <StyledCountdownCircle>
       <div id='countdown-number'>{countdown || 0}</div>
       <svg>
         <circle r='10.8' cx='12' cy='12'></circle>
