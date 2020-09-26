4255d46d59911e30621c742cd98d96c71d7de66d
mambans <perssons1996@gmail.com>
2020-09-26 23:59:14 +0200

    Completely changed AioFeeds logo/favicon.

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 
 import './Home.scss';
-import { BlurOverlay, LogoText, WelcomeContainer } from './StyledComponents';
+import { BlurOverlay, LogoText, WelcomeContainer, DevideLine } from './StyledComponents';
 
 export default () => {
   document.title = 'AioFeed';
@@ -13,14 +13,16 @@ export default () => {
         <WelcomeContainer>
           <LogoText>
             <img
-              src={`${process.env.PUBLIC_URL}/A/android-chrome-512x512.png`}
+              // src={`${process.env.PUBLIC_URL}/android-chrome-512x512.png`}
+              src={`${process.env.PUBLIC_URL}/logo_transparent_shadow.png`}
               alt='logo'
               id='logo'
             />
             <div>
               <h1>
-                <b>ioFeed</b>
+                <b>AioFeed</b>
               </h1>
+              <DevideLine />
               <p>
                 View Twitch & Youtube feeds and a Twitter list in one page, with Twitch
                 (live/offline/update) notifications.
