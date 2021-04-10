dd6be8780c654e82b3e59aab69000e06811f5fc6
mambans <perssons1996@gmail.com>
2021-04-10 12:22:00 +0200

    Changed custom hook 'useQuery' to also accept an url as param instead of just current location.

--- frontend/src/hooks/useQuery.js
+++ frontend/src/hooks/useQuery.js
@@ -1,3 +1,3 @@
 import { useLocation } from 'react-router-dom';
 
-export default () => new URLSearchParams(useLocation().search);
+export default (url) => new URLSearchParams(url || useLocation().search);
