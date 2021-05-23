b385f8d8ce7d6b511b92c21d9ab162d96b78baca
mambans <perssons1996@gmail.com>
2021-05-23 22:13:51 +0200

    Added a reusable <DropDown> menu component that uses react-portal to counter overflow problems. Moved individual feed links in navbar to a dropdown menu.

---
+++ frontend/src/components/navigation/DropDown.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { NavDropdown } from 'react-bootstrap';
+import styled from 'styled-components';
+
+import { PortalWithState } from 'react-portal';
+
+const StyledDropdownContainer = styled.div`
+  position: fixed;
+  left: ${({ triggerOffset, selfOffets }) =>
+    triggerOffset?.left - (selfOffets?.width - triggerOffset?.width) / 2}px;
+  top: ${({ triggerOffset }) => triggerOffset?.bottom}px;
+  background: var(--navigationbarBackground);
+  border-radius: 10px;
+  z-index: 2;
+  padding-top: 8px;
+
+  a {
+    color: var(--navTextColor);
+
+    &:last-child {
+      border-radius: 0 0 10px 10px;
+    }
+
+    svg {
+      margin-right: 5px;
+    }
+  }
+`;
+
+const DropdownContainer = ({ triggerRef, onClick, children }) => {
+  const [selfOffets, setSelfOffsets] = useState();
+  const [triggerOffset, setTriggerOffset] = useState();
+  const ref = useRef();
+
+  useEffect(() => {
+    setSelfOffsets(ref.current?.getBoundingClientRect());
+    setTriggerOffset(triggerRef.current?.getBoundingClientRect());
+  }, [triggerRef]);
+
+  return (
+    <StyledDropdownContainer
+      ref={ref}
+      triggerOffset={triggerOffset}
+      selfOffets={selfOffets}
+      onClick={onClick}
+    >
+      {children}
+    </StyledDropdownContainer>
+  );
+};
+
+export default ({ title, children }) => {
+  const triggerRef = useRef();
+
+  return (
+    <PortalWithState closeOnOutsideClick closeOnEsc>
+      {({ openPortal, closePortal, isOpen, portal }) => (
+        <>
+          <NavDropdown title={title} onClick={openPortal} ref={triggerRef} />
+          {portal(
+            <DropdownContainer triggerRef={triggerRef} onClick={closePortal}>
+              {children}
+            </DropdownContainer>
+          )}
+        </>
+      )}
+    </PortalWithState>
+  );
+};
--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -5,6 +5,7 @@ import { CSSTransition } from 'react-transition-group';
 import styled from 'styled-components';
 import { FaAngleRight, FaAngleLeft, FaTwitch, FaYoutube } from 'react-icons/fa';
 import { MdVideocam, MdStar } from 'react-icons/md';
+import { NavDropdown } from 'react-bootstrap';
 
 import RenderNotifications from './../notifications';
 import NavigationContext from './NavigationContext';
@@ -14,6 +15,7 @@ import GameSearchBar from '../twitch/categoryTopStreams/GameSearchBar';
 import ChannelSearchList from './../twitch/channelList/index';
 import NavExpandingSides from './NavExpandingSides';
 import VodsContext, { VodsProvider } from '../twitch/vods/VodsContext';
+import DropDown from './DropDown';
 
 const StyledNavbar = styled(Navbar)`
   display: flex;
@@ -30,6 +32,7 @@ const StyledNav = styled(Nav)`
   a {
     display: flex;
     align-items: center;
+    white-space: nowrap;
 
     svg {
       margin: 0 2px;
@@ -81,26 +84,33 @@ export default () => {
             <Nav.Link as={NavLink} to='/feed' activeClassName='active'>
               Feed
             </Nav.Link>
-            <Nav.Link as={NavLink} to='/favorites' activeClassName='active'>
-              <MdStar size={16} color='rgb(255,255,0)' />
-              Favorites
+            <Nav.Link as={NavLink} to='/category' activeClassName='active'>
+              Top Streams
             </Nav.Link>
-            <Nav.Link as={NavLink} to='/live' activeClassName='active'>
-              <FaTwitch size={16} color='rgb(118, 65, 198)' />
-              Live
-            </Nav.Link>
-            <Nav.Link as={NavLink} to='/youtube' activeClassName='active'>
-              <FaYoutube size={16} color='rgb(255, 0, 0)' />
-              YouTube
-            </Nav.Link>
-            <Nav.Link as={NavLink} to='/vods' activeClassName='active'>
-              <MdVideocam size={16} color='rgb(118, 65, 198)' />
-              Vods
-            </Nav.Link>
+            <DropDown title='Individual Feeds'>
+              {' '}
+              <NavDropdown.Item as={NavLink} to='/favorites'>
+                <MdStar size={16} color='rgb(255,255,0)' />
+                Favorites
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to='/live'>
+                <FaTwitch size={16} color='rgb(118, 65, 198)' />
+                Live
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to='/youtube'>
+                <FaYoutube size={16} color='rgb(255, 0, 0)' />
+                YouTube
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to='/vods'>
+                <MdVideocam size={16} color='rgb(118, 65, 198)' />
+                Vods
+              </NavDropdown.Item>
+            </DropDown>
           </StyledNav>
           <FaAngleRight className='arrow' size={20} />
           <FaAngleRight className='arrow shadow' size={20} />
         </NavExpandingSides>
+
         <NavExpandingSides side='right'>
           <FaAngleLeft className='arrow' size={20} />
           <FaAngleLeft className='arrow shadow' size={20} />
