279459a3a3854bbf6257ea8cff23c38d4ac1818d
mambans <perssons1996@gmail.com>
2021-07-13 21:31:36 +0200

    Fixed occasional 'RangeError: invalid array length ' error.

--- frontend/src/components/twitch/LoadingBoxes.js
+++ frontend/src/components/twitch/LoadingBoxes.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { LoadingVideoElement } from './StyledComponents';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-const LoadingBoxed = ({ amount, type, load = true }) => {
-  const array = Array.apply(null, Array(amount)).map((x, i) => i);
+const LoadingBoxes = ({ amount = 3, type, load = true }) => {
+  const array = Array.apply(null, Array(Math.floor(Math.max(amount, 1)))).map((x, i) => i);
 
   if (load) {
     return (
       <TransitionGroup component={null}>
-        {array.map((index) => (
-          <CSSTransition key={index} timeout={250} classNames='fade-250ms' unmountOnExit>
+        {array.map((v) => (
+          <CSSTransition key={v} timeout={250} classNames='fade-250ms' unmountOnExit>
             <LoadingVideoElement type={type} />
           </CSSTransition>
         ))}
@@ -19,4 +19,4 @@ const LoadingBoxed = ({ amount, type, load = true }) => {
   }
   return null;
 };
-export default LoadingBoxed;
+export default LoadingBoxes;
