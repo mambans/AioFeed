7c377d2431cd14ee758e935eb0c4afbf6b37e471
mambans <perssons1996@gmail.com>
2021-08-29 14:56:11 +0200

    Adding automated deploys from github.
    Added .travis.yml config file.

--- frontend/src/components/app/App.test.js
+++ frontend/src/components/app/App.test.js
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
+beforeAll(() => {
+  window.scrollTo = () => {};
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
