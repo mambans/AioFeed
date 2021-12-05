4f80d86139acc000960cf0641d551ce236ef7214
mambans <perssons1996@gmail.com>
2021-12-05 13:45:06 +0100

    .

--- frontend/src/components/navigation/NavExpandingSides.js
+++ frontend/src/components/navigation/NavExpandingSides.js
@@ -3,7 +3,7 @@ import { Nav } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const NavExpandingSidesContainer = styled(Nav)`
-  min-width: 400px;
+  min-width: 410px;
   align-items: center;
   justify-content: ${({ side }) => (side === 'right' ? 'flex-end' : 'start')};
   z-index: 5;
@@ -15,7 +15,7 @@ const NavExpandingSides = styled.div`
   }
 
   border-radius: ${({ side }) => (side === 'right' ? '25px 0 0 25px' : '0 25px 25px 0')};
-  width: 400px;
+  width: 410px;
   transition: width 250ms cubic-bezier(0.46, 0.03, 0.52, 0.96);
   height: inherit;
   overflow: hidden;
