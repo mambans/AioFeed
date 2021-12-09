e9b08f0eb2ed9e8af92d6ff25f834ee85e612b7b
mambans <perssons1996@gmail.com>
2021-12-09 20:33:20 +0100

    .

--- frontend/src/components/sharedComponents/ExpandableSection.js
+++ frontend/src/components/sharedComponents/ExpandableSection.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { StyledExpandSection } from './sharedStyledComponents';
 
-const ExpandableSection = ({ collapsed, children, ...rest }) => {
+const ExpandableSection = ({ collapsed, children, isOpened, ...rest }) => {
   const ref = useRef();
   const [height, setHeight] = useState({ collapsed });
   const timer = useRef();
 
   useEffect(() => {
     clearTimeout(timer.current);
-    setHeight({ height: ref.current?.getBoundingClientRect()?.height + 'px' });
+    setHeight({ height: ref.current?.getBoundingClientRect()?.height + 'px', isOpened: false });
     requestAnimationFrame(() => {
-      setHeight({ height: ref.current?.getBoundingClientRect()?.height + 'px', collapsed });
+      setHeight({
+        height: ref.current?.getBoundingClientRect()?.height + 'px',
+        collapsed,
+        isOpened: false,
+      });
     });
 
     timer.current = setTimeout(() => {
       // requestAnimationFrame(() => {
       setHeight((c) => {
-        if (!c.collapsed) return { height: 'unset', collapsed: c.collapsed };
+        if (!c.collapsed) return { height: 'unset', collapsed: c.collapsed, isOpened: true };
         return c;
       });
       // });
@@ -24,7 +28,12 @@ const ExpandableSection = ({ collapsed, children, ...rest }) => {
   }, [collapsed]);
 
   return (
-    <StyledExpandSection {...rest} height={height?.height} collapsed={String(height?.collapsed)}>
+    <StyledExpandSection
+      {...rest}
+      isOpened={height?.isOpened}
+      height={height?.height}
+      collapsed={String(height?.collapsed)}
+    >
       <div ref={ref}>{children}</div>
     </StyledExpandSection>
   );
