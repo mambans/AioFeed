e05123f600dc8a7da68054df38c284031682dd22
mambans <perssons1996@gmail.com>
2022-01-29 17:04:27 +0100

    .

--- frontend/src/pages/sharedComponents/sharedStyledComponents.js
+++ frontend/src/pages/sharedComponents/sharedStyledComponents.js
@@ -70,7 +70,7 @@ export const StyledVideoContainer = styled.div`
   &.videoFadeSlide-enter {
     opacity: 0;
     margin-left: ${({ feedVideoSizeProps }) =>
-      -feedVideoSizeProps.totalWidth - feedVideoSizeProps.margin}px !important;
+      -(feedVideoSizeProps.totalWidth - feedVideoSizeProps.margin)}px !important;
     margin-right: 0px !important;
     transition: opacity 500ms, margin-left 750ms, width 750ms, margin-right 750ms, max-width 750ms,
       transform 750ms;
@@ -99,7 +99,8 @@ export const StyledVideoContainer = styled.div`
 
   &.videoFadeSlide-exit-active {
     opacity: 0;
-    margin-left: ${({ feedVideoSizeProps }) => -feedVideoSizeProps.totalWidth}px !important;
+    margin-left: ${({ feedVideoSizeProps }) =>
+      -(feedVideoSizeProps.totalWidth - feedVideoSizeProps.margin)}px !important;
     margin-right: 0px !important;
     transition: opacity 500ms ease 250ms, margin-left 750ms, width 750ms, margin-right 750ms,
       max-width 750ms, transform 750ms;
