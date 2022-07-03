02a1f2599cc38b0a15a867aeb5d48cc7d379bb75
mambans <perssons1996@gmail.com>
2022-07-03 19:58:05 +0200

    Created TS (typescript branch)

---
+++ frontend/deploy-ts.sh
@@ -0,0 +1,11 @@
+echo "\e[32mDeploy Started: `date`\e[0m"
+
+aws cloudformation deploy --template-file cloudformation.yaml --stack-name aiofeed-frontend-ts --no-fail-on-empty-changeset
+
+set -e
+
+npm run build
+
+aws s3 sync --delete ./build s3://ts.aiofeed.com
+
+echo "\e[42mDeploy Done: `date`\e[0m"
