5b735196f68aa64c7d3c5df32b12bb914e8ba601
Mambans <perssons1996@gmail.com>
2022-08-18 11:32:00 +0200

    .

--- frontend/src/pages/twitch/player/ContextMenu.js
+++ frontend/src/pages/twitch/player/ContextMenu.js
@@ -1,55 +1,55 @@
-import React from 'react';
-import { MdHighQuality } from 'react-icons/md';
-import ContextMenu, { ContextMenuDropDown } from './ContextMenuWrapper';
-import { MdOutlineHighQuality } from 'react-icons/md';
+import React from "react";
+import { MdHighQuality } from "react-icons/md";
+import ContextMenu, { ContextMenuDropDown } from "./ContextMenuWrapper";
+import { MdOutlineHighQuality } from "react-icons/md";
 
 const PlayerContextMenu = ({
-  PlayerUIControlls,
-  TwitchPlayer,
-  showAndResetTimer,
+	PlayerUIControlls,
+	TwitchPlayer,
+	showAndResetTimer,
 
-  children,
+	children,
 }) => {
-  const Qualities = () => {
-    if (!TwitchPlayer?.getQualities() || !TwitchPlayer?.getQualities()?.length) return null;
+	const Qualities = () => {
+		if (!TwitchPlayer?.getQualities() || !TwitchPlayer?.getQualities()?.length) return null;
 
-    return (
-      <>
-        <li onClick={() => TwitchPlayer?.setQuality('chunked')}>
-          <MdHighQuality size={24} />
-          {'Max quality (Source)'}
-        </li>
-        <ContextMenuDropDown
-          trigger={
-            <>
-              <MdOutlineHighQuality size={24} />
-              Qualities
-            </>
-          }
-        >
-          {TwitchPlayer?.getQualities()
-            ?.filter((q) => q.group !== 'chunked')
-            ?.map((q, index) => (
-              <li key={q.name || index} onClick={() => TwitchPlayer?.setQuality(q.group)}>
-                {q.name}
-              </li>
-            ))}
-        </ContextMenuDropDown>
-      </>
-    );
-  };
+		return (
+			<>
+				<div onClick={() => TwitchPlayer?.setQuality("chunked")}>
+					<MdHighQuality size={24} />
+					{"Max quality (Source)"}
+				</div>
+				<ContextMenuDropDown
+					trigger={
+						<>
+							<MdOutlineHighQuality size={24} />
+							Qualities
+						</>
+					}
+				>
+					{TwitchPlayer?.getQualities()
+						?.filter((q) => q.group !== "chunked")
+						?.map((q, index) => (
+							<div key={q.name || index} onClick={() => TwitchPlayer?.setQuality(q.group)}>
+								{q.name}
+							</div>
+						))}
+				</ContextMenuDropDown>
+			</>
+		);
+	};
 
-  return (
-    <ContextMenu
-      outerContainer={PlayerUIControlls}
-      showAndResetTimer={showAndResetTimer}
-      children={
-        <>
-          <Qualities />
-          {children}
-        </>
-      }
-    />
-  );
+	return (
+		<ContextMenu
+			outerContainer={PlayerUIControlls}
+			showAndResetTimer={showAndResetTimer}
+			children={
+				<>
+					<Qualities />
+					{children}
+				</>
+			}
+		/>
+	);
 };
 export default PlayerContextMenu;
