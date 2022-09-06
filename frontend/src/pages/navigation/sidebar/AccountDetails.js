106c70c37d00983ee82329f04a5482ec24efef68
mambans <perssons1996@gmail.com>
2022-09-06 22:28:51 +0200

    .

--- frontend/src/pages/navigation/sidebar/AccountDetails.js
+++ frontend/src/pages/navigation/sidebar/AccountDetails.js
@@ -1,19 +1,13 @@
 import React, { useContext } from 'react';
 
 import AccountContext from './../../account/AccountContext';
-import { StyledProfileImg, ProfileImgContainer, InlineError } from './StyledComponents';
+import { InlineError } from './StyledComponents';
 
 const AccountDetails = () => {
   const { user } = useContext(AccountContext);
 
   return (
     <>
-      <ProfileImgContainer>
-        <StyledProfileImg
-          src={user?.profile_image || `${process.env.PUBLIC_URL}/images/webp/placeholder.webp`}
-          alt=''
-        />
-      </ProfileImgContainer>
       <h1 style={{ fontSize: '2rem', textAlign: 'center' }} title='Username'>
         {user?.username}
       </h1>
