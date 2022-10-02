879c904b21f4407f29967c70a64f0123ee9a5a03
mambans <perssons1996@gmail.com>
2022-10-02 15:16:22 +0200

    .

--- frontend/src/pages/twitch/loginNameFormat.js
+++ frontend/src/pages/twitch/loginNameFormat.js
@@ -2,9 +2,9 @@ const loginNameFormat = (data, returnUserName = false) => {
   const { user_name, user_login, login, broadcaster_name, name, display_name, to_login, to_name } =
     data || {};
   const userName = display_name || broadcaster_name || user_name || name || user_login || to_name;
+  const englishName = login || user_login || to_login || userName;
 
-  if (returnUserName) return userName;
-  const englishName = login || user_login || to_login;
+  if (returnUserName) return englishName;
   const rightName =
     userName?.toLowerCase() !== englishName?.toLowerCase() && englishName ? `(${englishName})` : '';
 
