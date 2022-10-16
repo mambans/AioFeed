a46d2cc71289780776c35e6d96d5284306e1c443
mambans <perssons1996@gmail.com>
2022-10-16 02:27:30 +0200

    .

--- frontend/src/pages/myLists/GetVideoInfo.js
+++ frontend/src/pages/myLists/GetVideoInfo.js
@@ -1,5 +1,6 @@
-import { getLocalstorage, chunk, setLocalStorage } from '../../util';
-import { YOUTUBE_INSTANCE } from '../youtube/API';
+import axios from 'axios';
+import { getLocalstorage, chunk, setLocalStorage, getCookie } from '../../util';
+import YoutubeAPI from '../youtube/API';
 // import YoutubeAPI from '../youtube/API';
 
 const GetVideoInfo = async ({ videos = [] }) => {
@@ -30,58 +31,86 @@ const GetVideoInfo = async ({ videos = [] }) => {
     return cachedVideos;
   }
 
-  const newVideosDetails = Boolean(unCachedFullyVideos?.length)
-    ? await Promise.all(
-        chunk(unCachedFullyVideos, 50).map(
-          async (chunk) =>
-            await YOUTUBE_INSTANCE.get(
-              `https://www.googleapis.com/youtube/v3/videos?part=contentDetails,snippet&id=${chunk}`
-            )
-              .then((res) =>
-                res.data.items.map((item) => ({
-                  ...item,
-                  contentDetails: {
-                    ...item.contentDetails,
-                    upload: { videoId: item.id },
-                  },
-                }))
-              )
-              .catch((e) => null)
+  const newVideosDetails = await Promise.all(
+    chunk(unCachedFullyVideos, 50).map(async (chunk) => {
+      // await YOUTUBE_INSTANCE.get(
+      //   `https://www.googleapis.com/youtube/v3/videos?part=contentDetails,snippet&id=${chunk}`
+      // )
+      //   .then((res) =>
+      //     res.data.items.map((item) => ({
+      //       ...item,
+      //       contentDetails: {
+      //         ...item.contentDetails,
+      //         upload: { videoId: item.id },
+      //       },
+      //     }))
+      //   )
+      //   .catch((e) => null)
 
-          // YoutubeAPI.getVideoInfo(`?part=contentDetails,snippet&id=${chunk}`)
-          //   .then((res) =>
-          //     res.data.items.map((item) => ({
-          //       ...item,
-          //       contentDetails: {
-          //         ...item.contentDetails,
-          //         upload: { videoId: item.id },
-          //       },
-          //     }))
-          //   )
-          //   .catch((e) => null)
-          // await axios
-          //   .get(
-          //     `https://www.googleapis.com/youtube/v3/videos?part=contentDetails,snippet&id=${chunk}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
-          //     {
-          //       headers: {
-          //         Accept: 'application/json',
-          //         Authorization: `Bearer ${getCookie('Youtube-access_token')}`,
-          //       },
-          //     }
-          //   )
-          //   .then((res) =>
-          //     res.data.items.map((item) => ({
-          //       ...item,
-          //       contentDetails: {
-          //         ...item.contentDetails,
-          //         upload: { videoId: item.id },
-          //       },
-          //     }))
-          //   )
-          //   .catch((e) => null)
+      const asd = await YoutubeAPI.getVideoInfo({ part: 'contentDetails,snippet', id: chunk })
+        .then((res) =>
+          res.data.items.map((item) => ({
+            ...item,
+            contentDetails: {
+              ...item.contentDetails,
+              upload: { videoId: item.id },
+            },
+          }))
         )
-      ).then((res) => res.flat(1))
-    : CachedFullyVideos;
+        .catch((e) => null);
+      console.log('asd:', asd);
+
+      const asd2 = await YoutubeAPI.getVideoInfo({ part: ['contentDetails', 'snippet'], id: chunk })
+        .then((res) =>
+          res.data.items.map((item) => ({
+            ...item,
+            contentDetails: {
+              ...item.contentDetails,
+              upload: { videoId: item.id },
+            },
+          }))
+        )
+        .catch((e) => null);
+      console.log('asd2:', asd2);
+
+      const asd3 = await YoutubeAPI.getVideoInfo({
+        part: 'contentDetails,snippet',
+        id: chunk.join(','),
+      })
+        .then((res) =>
+          res.data.items.map((item) => ({
+            ...item,
+            contentDetails: {
+              ...item.contentDetails,
+              upload: { videoId: item.id },
+            },
+          }))
+        )
+        .catch((e) => null);
+      console.log('asd3:', asd3);
+
+      return await axios
+        .get(
+          `https://www.googleapis.com/youtube/v3/videos?part=contentDetails,snippet&id=${chunk}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
+          {
+            headers: {
+              Accept: 'application/json',
+              Authorization: `Bearer ${getCookie('Youtube-access_token')}`,
+            },
+          }
+        )
+        .then((res) =>
+          res.data.items.map((item) => ({
+            ...item,
+            contentDetails: {
+              ...item.contentDetails,
+              upload: { videoId: item.id },
+            },
+          }))
+        )
+        .catch((e) => null);
+    })
+  ).then((res) => res.flat(1));
 
   if (Boolean(fullyCachedVideos.items.length) && Boolean(newVideosDetails.length)) {
     setLocalStorage('Cached_SavedYoutubeVideos', {
