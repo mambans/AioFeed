9ec64f0408367026e502e07d96f285370447f347
mambans <perssons1996@gmail.com>
2022-12-21 19:24:39 +0100

    .

--- frontend/src/pages/twitch/player/Player.js
+++ frontend/src/pages/twitch/player/Player.js
@@ -434,7 +434,7 @@ const Player = () => {
       </ContextMenuDropDown>
     );
   };
-  console.log('streamInfo.tags:', streamInfo.tags);
+  console.log('streamInfo.tags:', streamInfo?.tags);
 
   return (
     <VideoAndChatContainer
