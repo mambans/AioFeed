0ed93dcea4ae05b0414fd447f17a25ca1bd69944
mambans <perssons1996@gmail.com>
2023-09-20 20:02:35 +0200

    .

--- frontend/src/pages/app/Styles.tsx
+++ frontend/src/pages/app/Styles.tsx
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const Background = styled.div`
 	position: fixed;
 	inset: 0;
-
 	background: ${({ theme }) => theme.colors.background};
+	z-index: -1;
 `;
