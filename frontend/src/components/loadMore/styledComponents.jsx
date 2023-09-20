e2b31206177a0fb87cf16fcd65b74ff8ae059baa
mambans <perssons1996@gmail.com>
2023-09-20 19:52:38 +0200

    .

--- frontend/src/components/loadMore/styledComponents.jsx
+++ frontend/src/components/loadMore/styledComponents.jsx
@@ -1,62 +1,63 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledLoadmore = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  max-width: 100%;
-  padding-bottom: 10px;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	max-width: 100%;
+	padding-bottom: 10px;
+	position: relative;
 
-  &:hover {
-    div:not(.button) {
-      opacity: 1;
-      transition: opacity 200ms;
-    }
-  }
+	&:hover {
+		div:not(.button) {
+			opacity: 1;
+			transition: opacity 200ms;
+		}
+	}
 
-  div:not(.button).line {
-    background: var(--subFeedHeaderBorder);
-    height: 1px;
-    opacity: 0.75;
-    transition: opacity 200ms;
-    flex-grow: 1;
-  }
+	div:not(.button).line {
+		background: var(--subFeedHeaderBorder);
+		height: 1px;
+		opacity: 0.75;
+		transition: opacity 200ms;
+		flex-grow: 1;
+	}
 
-  div.button {
-    display: flex;
-    width: max-content;
-    cursor: pointer;
-    margin: 0;
-    font-weight: bold;
-    color: var(--textColor2);
-    padding: 0px 15px;
-    transition: color 200ms, padding 200ms;
-    white-space: nowrap;
+	div.button {
+		display: flex;
+		width: max-content;
+		cursor: pointer;
+		margin: 0;
+		font-weight: bold;
+		color: var(--textColor2);
+		padding: 0px 15px;
+		transition: color 200ms, padding 200ms;
+		white-space: nowrap;
 
-    &:hover {
-      color: white;
-      padding: 0px 25px;
-    }
-  }
+		&:hover {
+			color: white;
+			padding: 0px 25px;
+		}
+	}
 
-  #reset {
-    cursor: pointer;
-    color: var(--textColor2);
-    margin-left: 5px;
-    transition: color 200ms, margin 200ms, stroke 200ms;
+	#reset {
+		cursor: pointer;
+		color: var(--textColor2);
+		margin-left: 5px;
+		transition: color 200ms, margin 200ms, stroke 200ms;
 
-    path {
-      transition: stroke 200ms;
-      stroke: var(--textColor2);
-    }
+		path {
+			transition: stroke 200ms;
+			stroke: var(--textColor2);
+		}
 
-    &:hover {
-      color: white;
-      margin-left: 10px;
+		&:hover {
+			color: white;
+			margin-left: 10px;
 
-      path {
-        stroke: white;
-      }
-    }
-  }
+			path {
+				stroke: white;
+			}
+		}
+	}
 `;
