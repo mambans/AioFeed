a4e889f8ea3759ca6300523b308adab2071122ec
mambans <perssons1996@gmail.com>
2023-10-25 20:19:38 +0200

    #0

--- frontend/src/pages/navigation/sidebar/ToggleButton.jsx
+++ frontend/src/pages/navigation/sidebar/ToggleButton.jsx
@@ -1,59 +1,43 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
 
-import { StyledToggleButton } from './StyledComponents';
-import ToolTip from '../../../components/tooltip/ToolTip';
+import { StyledToggleButton } from "./StyledComponents";
 
-const ToggleButton = ({
-  setEnable,
-  enabled,
-  label,
-  tooltip,
-  icon,
-  buttonsperrow,
-  scrollIntoView,
-  smallerIcons,
-  serviceName,
-  disabled,
-}) => {
-  // const [checked, setChecked] = useState(enabled || false);
-  const timeout = useRef();
+const ToggleButton = ({ setEnable, enabled, label, tooltip, icon, buttonsperrow, scrollIntoView, smallerIcons, serviceName, disabled }) => {
+	// const [checked, setChecked] = useState(enabled || false);
+	const timeout = useRef();
 
-  function handleChange() {
-    console.log('ToggleButton onclick:');
-    const newEnabled = !enabled;
-    setEnable(newEnabled);
-    clearTimeout(timeout.current);
-    if (scrollIntoView && newEnabled === true) {
-      timeout.current = setTimeout(() => {
-        const element = document.getElementById(`${label || serviceName}Header`);
-        element &&
-          element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
-      }, 150);
-    }
-  }
+	function handleChange() {
+		console.log("ToggleButton onclick:");
+		const newEnabled = !enabled;
+		setEnable(newEnabled);
+		clearTimeout(timeout.current);
+		if (scrollIntoView && newEnabled === true) {
+			timeout.current = setTimeout(() => {
+				const element = document.getElementById(`${label || serviceName}Header`);
+				element && element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+			}, 150);
+		}
+	}
 
-  useEffect(() => {
-    return () => {
-      clearTimeout(timeout.current);
-    };
-  }, []);
+	useEffect(() => {
+		return () => {
+			clearTimeout(timeout.current);
+		};
+	}, []);
 
-  return (
-    <ToolTip tooltip={tooltip} width='max-content'>
-      <StyledToggleButton
-        onClick={handleChange}
-        disabled={disabled}
-        variant='dark'
-        buttonsperrow={buttonsperrow}
-        enabled={String(enabled)}
-      >
-        {icon}
-        {React.Children?.map(smallerIcons, (icon) =>
-          React.isValidElement(icon) ? React.cloneElement(icon, { className: 'smallIcon' }) : icon
-        )}
-      </StyledToggleButton>
-    </ToolTip>
-  );
+	return (
+		<StyledToggleButton
+			onClick={handleChange}
+			disabled={disabled}
+			variant="dark"
+			buttonsperrow={buttonsperrow}
+			enabled={String(enabled)}
+			tooltip={tooltip}
+		>
+			{icon}
+			{React.Children?.map(smallerIcons, (icon) => (React.isValidElement(icon) ? React.cloneElement(icon, { className: "smallIcon" }) : icon))}
+		</StyledToggleButton>
+	);
 };
 
 export default ToggleButton;
